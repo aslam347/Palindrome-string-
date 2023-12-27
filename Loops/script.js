6a27cc2536b4d67ec45ecbf2422fd429d61bf460
error-makes-clever <aslamthanjavur10@gmail.com>
2023-12-27 19:12:47 +0530

    loop basic

---
+++ Loops/script.js
@@ -0,0 +1,45 @@
+// for loop
+let i
+for(i = 1; i<=5; i++){
+
+console.log(i)
+
+}
+
+// 10 to 1
+for(i = 10; i>=1; i--){
+    console.log(i)
+}
+
+// while loop
+
+let j = 15
+
+while(j>=11){
+
+    console.log(j)
+     j--;
+}
+
+// isNaN is not a number 
+//while(true){
+
+  //  let num = Number(prompt('enter the number'))
+
+    //if(!isNaN(num)){
+        
+      //  break;
+  //  }
+//}
+
+
+
+let k = 1
+
+for( k = 1 ; k<=10; i++){
+
+if(k%3==0)
+continue 
+
+
+}
